b0fa64ae07c0e62dee35ff075d11ee8e6fda7da2
Yuxian Gu <guyx21@mails.tsinghua.edu.cn>
2024-03-16 16:45:49 +0800

    update install

--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
 export NCCL_DEBUG=""
 pip3 install transformers
-pip3 install torch==2.0.1
+pip3 install torch
 pip3 install deepspeed
-pip3 install torchvision==0.15.2
 pip3 install nltk
 pip3 install numerize
 pip3 install rouge-score
@@ -11,13 +10,11 @@ pip3 install rich
 pip3 install accelerate
 pip3 install datasets
 pip3 install sentencepiece
-pip3 install protobuf==3.20.3
 pip3 install peft
 pip3 install matplotlib
 
-ln -s /mnt/yuxian/sps/results_residual/ results
-ln -s /mnt/yuxian/sps/processed_data/ processed_data
-ln -s /mnt/yuxian/data downstream_data
-ln -s /mnt/yuxian/checkpoints checkpoints
-ln -s /mnt/yuxian/pretrain_data pretrain_data
-ln -s /mnt/yuxian/azcopy_linux_amd64_10.20.1/azcopy azcopy
+ln -s /data/guyuxian/sps/results/ results
+ln -s /data/guyuxian/sps/processed_data/ processed_data
+ln -s /data/guyuxian/data downstream_data
+ln -s /data/guyuxian/checkpoints checkpoints
+ln -s /data/guyuxian/pretrain_data pretrain_data
